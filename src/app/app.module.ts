3cb5d6f32d212b1af7e5354d04864a3ff979c736
Kevin Quesada Elizondo <kevinque01@gmail.com>
2022-12-27 18:19:23 -0600

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,8 @@ import { FormularioCineComponent } from './cines/formulario-cine/formulario-cine
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { MapaComponent } from './utilidades/mapa/mapa.component';
 import { FormularioPeliculaComponent } from './peliculas/formulario-pelicula/formulario-pelicula.component';
-import { SelectorMultipleComponent } from './utilidades/selector-multiple/selector-multiple.component'
+import { SelectorMultipleComponent } from './utilidades/selector-multiple/selector-multiple.component';
+import { AutocompleteActoresComponent } from './actores/autocomplete-actores/autocomplete-actores.component'
 
 @NgModule({
   declarations: [
@@ -61,7 +62,8 @@ import { SelectorMultipleComponent } from './utilidades/selector-multiple/select
     FormularioCineComponent,
     MapaComponent,
     FormularioPeliculaComponent,
-    SelectorMultipleComponent
+    SelectorMultipleComponent,
+    AutocompleteActoresComponent
   ],
   imports: [
     BrowserModule,
